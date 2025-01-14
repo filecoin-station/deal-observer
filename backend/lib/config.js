0f4ee8ab15fe73969a6e94ca5a6b9f19e67b5971
Nikolas Haimerl <nikolas.haimerl@dfinity.org>
2025-01-14 23:41:48 +0100

    poc claim retrieval

--- backend/lib/config.js
+++ backend/lib/config.js
@@ -1,18 +1,22 @@
 const {
-  RPC_URLS = 'https://api.node.glif.io/rpc/v0',
+  RPC_URLS = 'https://api.node.glif.io/rpc/v1',
   GLIF_TOKEN
 } = process.env
 
 const rpcUrls = RPC_URLS.split(',')
-const RPC_URL = rpcUrls[Math.floor(Math.random() * rpcUrls.length)]
-console.log(`Selected JSON-RPC endpoint ${RPC_URL}`)
+const GLIF_RPC = rpcUrls[Math.floor(Math.random() * rpcUrls.length)]
+console.log(`Selected JSON-RPC endpoint ${GLIF_RPC}`)
 
 const rpcHeaders = {}
-if (RPC_URL.includes('glif')) {
+if (GLIF_RPC.includes('glif')) {
   rpcHeaders.Authorization = `Bearer ${GLIF_TOKEN}`
 }
 
+// The types of events we want to watch out for 
+const EVENT_TYPES = ["claim"]
+
 export {
-  RPC_URL,
-  rpcHeaders
+  GLIF_RPC,
+  rpcHeaders,
+  EVENT_TYPES
 }
--- backend/lib/deal-observer.js
+++ backend/lib/deal-observer.js
@@ -1,11 +1,41 @@
 /** @import {Queryable} from '@filecoin-station/deal-observer-db' */
 /** @import {Provider} from 'ethers' */
 
-/**
- * Observe the built-in actor events on the Filecoin blockchain to discover new FIL+ deals
- * @param {Queryable} pgPool
- * @param {Provider} rpcProvider
- */
-export const observeBuiltinActorEvents = async (pgPool, rpcProvider) => {
-  // TBD
+import { EVENT_TYPES, GLIF_RPC } from './config.js'
+import { ActorEventFilter, LotusService } from './lotus/service.js'
+
+class DealObserver {
+  #pgPool
+  #lotusService
+  #rpcProviderURL
+  #cache
+
+  constructor(pgPool=null, rpcProviderURL = GLIF_RPC, chainHead = null) {
+    // TODO: Store events in pgPool
+    this.#pgPool = pgPool
+    this.#rpcProviderURL = rpcProviderURL
+    this.#cache = new Map()
+    this.#cache.set('chainHead', chainHead)
+  }
+
+  async build(){
+    this.#lotusService = await (new LotusService(this.#rpcProviderURL)).build()
+    if (!this.#cache.get('chainHead')) {
+      let chainHead = await this.#lotusService.getChainHead()
+      this.#cache.set('chainHead', chainHead)
+    }
+    return this
+  }
+
+  async observeBuiltinActorEvents(fromHeight=this.#cache.get("chainHead").Height, toHeight=this.#cache.get("chainHead").Height,eventTypes=EVENT_TYPES) {
+    return this.#lotusService.getActorEvents(new ActorEventFilter(fromHeight, toHeight, eventTypes))
+  }
 }
+
+export {
+  DealObserver
+}
+
+(new DealObserver()).build().then((dealObserver) => {
+  dealObserver.observeBuiltinActorEvents(4618314,4618314).then((events) => {console.log(events)})
+})
