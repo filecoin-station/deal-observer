a5e94cb3d88a2eef02cd29526df1faf640ba8ff5
Nikolas Haimerl <nikolas.haimerl@dfinity.org>
2025-01-15 09:48:40 +0100

    add transformer for claim events

---
+++ backend/lib/lotus/transform.js
@@ -0,0 +1,47 @@
+import { EVENT_TYPES, GLIF_RPC } from '../config.js'
+import { base64pad } from 'multiformats/bases/base64'
+import { encode as cborEncode, decode as cborDecode } from '@ipld/dag-cbor'
+import { decode as jsonDecode, encode as jsonEncode } from '@ipld/dag-json'
+import { request } from 'undici'
+import { readFile } from 'node:fs/promises'
+import { fromDSL } from '@ipld/schema/from-dsl.js'
+import { create } from '@ipld/schema/typed.js'
+
+class Transformer {
+  #claimEventTransformer
+
+  async build () {
+    const schemaPath = new URL('../lotus/builtin-actor-events-schemas.ipldsch', import.meta.url)
+    const schemaDsl = await readFile(schemaPath, 'utf8')
+    const schemaDmt = fromDSL(schemaDsl)
+    this.#claimEventTransformer = this.#createTransformer(schemaDmt, 'ClaimEvent')
+    return this
+  }
+
+  #createTransformer (schemaDmt, name) {
+    return create(schemaDmt, name)
+  }
+
+  transform (typeName, data) {
+    switch (typeName) {
+      case 'ClaimEvent':
+        return this.#claimEventTransformer.toTyped(data)
+      default:
+        console.log(`Unknown type ${typeName}`)
+    }
+  }
+}
+
+const decodeCborInBase64 = (data) => {
+  return cborDecode(base64pad.baseDecode(data))
+}
+
+const encodeCborInBase64 = (data) => {
+  return base64pad.baseEncode(cborEncode(data))
+}
+
+export {
+  decodeCborInBase64,
+  encodeCborInBase64,
+  Transformer
+}
