cf379aa2edc7f35ac2d1a1acc98c0efffe60f601
Nikolas Haimerl <nhaimerl@Nikolass-MacBook-Pro.local>
2025-01-16 15:45:47 +0100

    formatting

--- backend/lib/deal-observer.js
+++ backend/lib/deal-observer.js
@@ -1,25 +1,23 @@
 /** @import {Queryable} from '@filecoin-station/deal-observer-db' */
 /** @import {Provider} from 'ethers' */
 
-import { EVENT_TYPES, GLIF_RPC } from './config.js'
+import { EVENT_TYPES } from './config.js'
 import { ActorEventFilter, LotusService } from './rpc-service/service.js'
 
 class DealObserver {
   #pgPool
   #lotusService
-  #rpcProviderURL
   #cache
 
-  constructor (pgPool = null, rpcProviderURL = GLIF_RPC, chainHead = null) {
+  constructor (pgPool = null, chainHead = null) {
     // TODO: Store events in pgPool
     this.#pgPool = pgPool
-    this.#rpcProviderURL = rpcProviderURL
     this.#cache = new Map()
     this.#cache.set('chainHead', chainHead)
   }
 
   async build () {
-    this.#lotusService = await (new LotusService(this.#rpcProviderURL)).build()
+    this.#lotusService = await (new LotusService()).build()
     if (!this.#cache.get('chainHead')) {
       const chainHead = await this.#lotusService.getChainHead()
       this.#cache.set('chainHead', chainHead)
@@ -35,7 +33,3 @@ class DealObserver {
 export {
   DealObserver
 }
-
-(new DealObserver()).build().then((dealObserver) => {
-  dealObserver.observeBuiltinActorEvents(4620803, 4620803).then((events) => { console.log(events) })
-})
