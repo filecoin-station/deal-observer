3ec7a0da4e1a55b378cfc5fce781d2a45e5df3b9
Nikolas Haimerl <113891786+NikolasHaimerl@users.noreply.github.com>
2025-01-24 14:22:48 +0100

    Update backend/bin/deal-observer-backend.js
    Co-authored-by: Miroslav Bajtoš <oss@bajtos.net>

--- backend/bin/deal-observer-backend.js
+++ backend/bin/deal-observer-backend.js
@@ -32,8 +32,7 @@ const dealObserverLoop = async (makeRpcRequest, pgPool) => {
       // If the storage is empty we start 2000 blocks into the past as that is the furthest we can go with the public glif rpc endpoints.
       const lastEpochStored = (await fetchDealWithHighestActivatedEpoch(pgPool)).height ?? currentChainHead.Height - maxPastEpochs
       // TODO: The free plan does not allow for fetching epochs older than 2000 blocks. We need to account for that.
-      /* eslint-disable no-unmodified-loop-condition */
-      for (let epoch = lastEpochStored + 1; lastEpochStored <= currentFinalizedChainHead; epoch++) {
+      for (let epoch = lastEpochStored + 1; epoch <= currentFinalizedChainHead; epoch++) {
         await observeBuiltinActorEvents(epoch, pgPool, makeRpcRequest)
       }
     } catch (e) {
