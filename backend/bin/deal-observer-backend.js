9f8049c69ec084deeaaa6654b8bf24517f0dce26
Nikolas Haimerl <113891786+NikolasHaimerl@users.noreply.github.com>
2025-02-07 11:43:59 +0000

    Update backend/bin/deal-observer-backend.js
    Co-authored-by: Julian Gruber <julian@juliangruber.com>

--- backend/bin/deal-observer-backend.js
+++ backend/bin/deal-observer-backend.js
@@ -128,7 +128,7 @@ export const lookUpPayloadCidsLoop = async (makeRpcRequest, getDealPayloadCid, p
       const numOfUnresolvedPayloadsCids = await countStoredActiveDealsWithUnresolvedPayloadCid(pgPool)
       if (INFLUXDB_TOKEN) {
         recordTelemetry('look_up_payload_cids_stats', point => {
-          point.intField('total_unresolved_payload_cids', numOfUnresolvedPayloadsCids)
+          point.intField('total_unresolved_payload_cids', numOfUnresolvedPayloadCids)
           point.intField('payload_cids_resolved', numOfPayloadsCidsResolved)
         })
       }
